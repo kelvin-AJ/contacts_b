6e7adfd20d285d2b55c9e6502ba9d0b18d149dda
Ola Ajibola <kelajibola@gmail.com>
2025-05-23 01:27:57 +0100

    add cors

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 require('dotenv').config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const router = require("./routes");
 const mongoDB = require("./data/database");
 const bodyParser = require("body-parser");
 
 
+
 const port = process.env.PORT || 3000
 
+app.use(cors)
 app.use("/", bodyParser.json())
 app.use("/", router)
 
