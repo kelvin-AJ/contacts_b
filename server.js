c1ddca8bceb30e8677a3002f997ac8bbe5897a02
Ola Ajibola <kelajibola@gmail.com>
2025-05-23 10:37:16 +0100

    add cors and bodyparser to global

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const bodyParser = require("body-parser");
 const port = process.env.PORT || 3000
 
 app.use(cors())
-app.use("/", bodyParser.json())
+app.use(bodyParser.json())
 app.use("/", router)
 
 
